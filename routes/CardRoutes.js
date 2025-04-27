70fbe11ef845fde635c97a5a880240ffba843f86
Abdelrahman <abdomohamed2200066@gmail.com>
2025-04-27 23:56:52 +0300

    kashier-webhook

--- routes/CardRoutes.js
+++ routes/CardRoutes.js
@@ -3,18 +3,11 @@ const router = express.Router();
 
 const { createOrder } = require("../controllers/OrderCardController");
 const { verifyToken, authorize } = require("../middleware/verifyToken");
-const {kashierWebhook } = require('../controllers/webhookController');
+const { kashierWebhook } = require("../controllers/webhookController");
 
-router.route("/order-card").post(verifyToken,createOrder);
+router.route("/order-card").post(verifyToken, createOrder);
 
 // إضافة مسار الويب هوك الخاص بـ Kashier
-router.post(
-  '/kashier-webhook',
-  express.raw({ type: 'application/json' }), // لمعالجة البيانات الخام
-  kashierWebhook
-);
-
-
-
+router.route("/kashier-webhook").post(kashierWebhook);
 
 module.exports = router;
