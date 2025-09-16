199cc86baa23e03e286f245615b36fd15458c97f
Abdelrahman1ll <abdomohamed2200066@gmail.com>
2025-09-16 19:05:32 +0300

    startServer

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@
 const connectDB = require("./config/dbConn");
 const app = require("./app");
 
-connectDB();
+async function startServer() {
+  try {
+    await connectDB();
 
-const PORT = process.env.PORT || 8000;
+    const PORT = process.env.PORT || 8000;
 
-const server = app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`.green);
-});
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`.green);
+    });
 
-// Handle rejection outside express
-process.on("unhandledRejection", (err) => {
-  console.log(`unhandledRejection Error: ${err.name} | ${err.message}`.red);
-  server.close(() => {
-    console.log("Server is shutting down...".red);
+    // Handle rejection outside express
+    process.on("unhandledRejection", (err) => {
+      console.log(`unhandledRejection Error: ${err.name} | ${err.message}`.red);
+      server.close(() => {
+        console.log("Server is shutting down...".red);
+        process.exit(1);
+      });
+    });
+  } catch (error) {
+    console.log(`Error connecting to database: ${error.message}`.red);
     process.exit(1);
-  });
-});
+  }
+}
+
+startServer();
